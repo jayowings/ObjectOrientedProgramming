8ea819a468c009bb3befb17b257998065c14a904
Jay Owings <jaydenowings@mail.weber.edu>
2021-04-12 08:31:36 -0600

    added objects with attributes

---
+++ README.md
@@ -0,0 +1,12 @@
+# Object Oriented Programming
+
+ - OOP is provides a means of structuring programs so that properties and behaviors are bundled into individual objects
+
+# Class
+
+ - blueprint of an object
+ - not the object itself
+
+# Object
+ - Attribute (Something that describes the object)
+    - Car Object (Attribute --> color --- car1 --> Blue)
---
+++ students.py
@@ -0,0 +1,21 @@
+class Student:
+    pass
+
+W01234 = Student()
+W01235 = Student()
+
+print(W01234)
+print(W01235)
+
+W01234.first = "Jayden"
+W01234.last = "Owings"
+W01234.email = "jaydenowings@mail.weber.edu"
+W01234.status = "Pass"
+
+W01235.first = "Waldo"
+W01235.last = "Wildcat"
+W01235.email = "waldowildcat@weber.edu"
+W01235.status = "Pass"
+
+print (W01234.email)
+print (W01235.email)
